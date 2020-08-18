f02ea39a47c5ace4257e4ac0428019e253f0ad9f
Akriti Singhal <akritisinghal@Akritis-MacBook-Pro.local>
2020-08-18 00:04:19 -0400

    React 4th lecture

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,102 @@
-import React, { Component } from 'react';
+//import React, { useState} from 'react';
+import React, { Component} from 'react';
 import './App.css';
 import Person from './Person/Person.js';
 
+//Class Component.
 class App extends Component {
+  state = {
+    persons : [
+      { name : 'Max', age: 29} ,
+      { name : 'Manu', age: 25} ,
+      { name : 'Stephanie', age: 29} 
+
+    ]
+  }
+
+  //buttonClickHandler = ()=> {
+    buttonClickHandler = (newName)=> {
+    //console.log("Button was clicked");
+    //this.state.person[0].name = "Maximilliab" -  Dont do this since react wont understand this
+    this.setState({
+      persons: [
+        //{ name : 'Maximillian', age: 29} ,
+        { name : newName, age: 29} ,
+        { name : 'Manu', age: 25} ,
+        { name : 'Stephanie', age: 29} 
+      ]
+    })
+  }
+
+  nameChangeHandler = (event)=>{
+    this.setState({
+      persons: [
+        //{ name : 'Maximillian', age: 29} ,
+        { name : 'Max', age: 29} ,
+        { name : event.target.value, age: 25} ,
+        { name : 'Stephanie', age: 29} 
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1> Hi, I am a react App</h1>
-        <Person name = "Max" age = "29" />
-        <Person name ="Manu" age = "25">Hobbies are dance and cricket </Person>
-        <Person name = "Stephanie" age = "30" />
+        <button onClick = {this.buttonClickHandler}>Click Button</button>
+        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} click = {this.buttonClickHandler.bind(this, 'Max!!!')} />
+        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} change = {this.nameChangeHandler}>Hobbies are dance and cricket </Person>
+        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
       </div>
     );
   }
 }
 
 export default App;
+
+//Functional Component.
+/*const app = (props)=>{
+  const [personState, setPersonState] = useState({
+    persons:[
+      {name: 'Max', age:28},
+      {name: 'Manu', age:30},
+      {name: 'Stephanie', age:29}
+
+    ]
+  });
+
+  const buttonClickHandler = ()=>{
+    setPersonState({
+      persons:[
+        {name: 'Maxilian', age:28},
+        {name: 'Manu', age:30},
+        {name: 'Stephanie', age:29}
+  
+      ]
+    });
+  };
+
+  
+  return (
+    <div className="app">
+      <h1> Hi, I am a react App</h1>
+      <button onClick = {buttonClickHandler}>Click Button</button>
+      <Person name = {personState.persons[0].name} age = {personState.persons[0].age} />
+      <Person name = {personState.persons[1].name} age = {personState.persons[1].age}>Hobbies are dance and cricket </Person>
+      <Person name = {personState.persons[2].name} age = {personState.persons[2].age} />
+    </div>
+  );
+} 
+
+ 
+
+  
+    
+  
+
+
+
+export default app;*/
 
 
 //This is the way of creating components in react. First of all we will import 
--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) =>{
     return(
-        <div>
-            <p>I am a {props.name} component. My age is {props.age} years.</p>
+        <div className = 'Person'>
+            <p onClick = {props.click}>I am a {props.name} component. My age is {props.age} years.</p>
             <p>{props.children}</p>
+            <input type = 'text' onChange={props.change} value ={props.name}/>
         </div>
     )
     
@@ -25,3 +27,33 @@ export default person;
 //Props children - this is the reserved property which is passed between the closing
 //and operning tags like <Person>Hobbies are dance and cricket</Person>.
 
+//What are states - states are the reserved properties that can be used in the
+//components/class which are extending component class. Refer to the props and states 
+//pdf.
+
+//Now with the help of state we will add event handlers in our code. We will add
+//a button and define a user  defined state which will handle the button event.
+
+//Now if you want to mnaipulate the state, you can do in your event handler.
+//this.setState will rerender the react DOM elemnent. 
+
+//What are hooks - These are the way of  managing states inside a functional  component.
+//States can only be used in class components thus we use hooks in functional  component.
+//For this first we will convert our app.js class component into a functional component and
+//we will use useState hook. useState is a function that has two parameters. One is
+// the state  and the other is setState which will allow you to change your 
+//state.
+
+//Thus there will be two  types of components- Stateful(smart components) - ones which will have states either its a functional or 
+//class component. Stateless(Presentational/Dumb Compoenents)- which doesnt  have states.
+
+//Passing Method Reference between components- Now if you want to make the click event on the p element of
+//Person.js, then you need to call the buttonClickHandler of App.js but since its a different compoennet you
+//cant do that. Thus what you can do is pass the method reference in the props of the components and
+//then use that props. You can also pass the value for this buttonclickhandler using bind function.
+
+//Two way binding - Now if you want to change the name based on what you are typing in text box, you can
+//do it by using a event object in the state.
+
+//Ading css for your Person component to make it look like a card.Create a person.css file and import it your Person.js
+//ie your person compoennt.
